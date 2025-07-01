c96ad4e456e6d97e102b222e8e84c3781e02da0a
brayanvx11 <delacruzbrayan649@gmail.com>
2025-07-01 02:45:57 -0600

    Actualizacion tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+export default { // Asegúrate de que sea 'export default' si es un proyecto Vite
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
